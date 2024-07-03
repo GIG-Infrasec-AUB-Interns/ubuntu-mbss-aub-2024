33d98c83d7181fda896bebc5c6c91269fdb14bfa
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-03 16:39:23 +0800

    fix: 1.1.2.1.2 test

--- tests/chap1/chap1_1/chap1_1_2/chap1_1_2_1/1_1_2_1_2.sh
+++ tests/chap1/chap1_1/chap1_1_2/chap1_1_2_1/1_1_2_1_2.sh
@@ -6,7 +6,7 @@
     echo "Ensuring nodev option set on /tmp partition (1.1.2.1.2)..."
     findmnt_output=$(findmnt -kn /tmp | grep -v nodev)
 
-    if [[ -z "$findmnt_output"]]; then
+    if [[ -z "$findmnt_output" ]]; then
         echo "Output from findmnt:"
         echo "$findmnt_output"
         echo "Audit Result: PASS"
