048dad37e4841c9192b2c275c3765950b5678b61
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-03 17:16:46 +0800

    feat: 1.1.2.1.2 test and remediation

--- tests/chap1/chap1_1/chap1_1_2/chap1_1_2_1/1_1_2_1_2.sh
+++ tests/chap1/chap1_1/chap1_1_2/chap1_1_2_1/1_1_2_1_2.sh
@@ -14,5 +14,25 @@
         echo "Output from findmnt:"
         echo "$findmnt_output"
         echo "Audit Result: FAIL"
+
+        read -p "Run remediation script for Test 1.1.2.1.2? (Y/N): " ANSWER
+        case "$ANSWER" in
+            [Yy]*)
+                echo "Commencing remediation for Test 1.1.2.1.2..."
+                
+                FIXES_SCRIPT="$(realpath fixes/chap1/chap1_1/chap1_1_2/chap1_1_2_1/1_1_2_1_2.sh)"
+                if [ -f "$FIXES_SCRIPT" ]; then
+                    chmod +x "$FIXES_SCRIPT"
+                    "$FIXES_SCRIPT"
+                else
+                    echo "Error: $FIXES_SCRIPT is not found."
+                fi
+                echo "For more information, please visit https://downloads.cisecurity.org/#/"
+                ;;
+            *)
+                echo "Remediation not commenced"
+                echo "For more information, please visit https://downloads.cisecurity.org/#/"
+                ;;
+        esac
     fi
 }
