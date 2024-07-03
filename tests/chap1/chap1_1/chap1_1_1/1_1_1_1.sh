12584924bb8a1230c79a0500cf3c1185c72e77b5
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-03 10:26:20 +0800

    feat: 1.1.1.2 test and remediation

--- tests/chap1/chap1_1/chap1_1_1/1_1_1_1.sh
+++ tests/chap1/chap1_1/chap1_1_1/1_1_1_1.sh
@@ -67,12 +67,11 @@
         [ -n "$l_output" ] && echo -e "\n- Correctly set:\n$l_output\n"
 
         # Remediation
-
         read -p "Run remediation script for Test 1.1.1.1? (Y/N): " ANSWER
         case "$ANSWER" in
             [Yy]*)
                 echo "Commencing remediation for Test 1.1.1.1..."
-                # Resolve absolute path to fixes/chap1/chap1_1/chap1_1_1/1_1_1_1.sh
+
                 FIXES_SCRIPT="$(realpath fixes/chap1/chap1_1/chap1_1_1/1_1_1_1.sh)"
                 if [ -f "$FIXES_SCRIPT" ]; then
                     chmod +x "$FIXES_SCRIPT"
