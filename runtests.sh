3765ad823be3b07188706a0d48b922d716e517dc
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-04 13:45:08 +0800

    feat: 1.1.2.7 tests

--- runtests.sh
+++ runtests.sh
@@ -39,16 +39,16 @@ echo "Testing config of /tmp (1.1.2.1)..."
 runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_1/*.sh
 echo "Testing config of /dev/shm (1.1.2.2)..."
 runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_2/*.sh
-# echo "Testing config of /home (1.1.2.3)..."
-# runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_3/*.sh
-# echo "Testing config of /var (1.1.2.4)..."
-# runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_4/*.sh
-# echo "Testing config of /var/tmp (1.1.2.5)..."
-# runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_5/*.sh
-# echo "Testing config of /var/log (1.1.2.6)..."
-# runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_6/*.sh
-# echo "Testing config of /var/log/audit (1.1.2.7)..."
-# runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_7/*.sh
+echo "Testing config of /home (1.1.2.3)..."
+runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_3/*.sh
+echo "Testing config of /var (1.1.2.4)..."
+runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_4/*.sh
+echo "Testing config of /var/tmp (1.1.2.5)..."
+runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_5/*.sh
+echo "Testing config of /var/log (1.1.2.6)..."
+runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_6/*.sh
+echo "Testing config of /var/log/audit (1.1.2.7)..."
+runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_7/*.sh
 
 
 # 1.3 Mandatory Access Control
