214ff22f2cfe029dd83943b517eea6cb9d26b5ea
root <root@Ubuntu22.04.myguest.virtualbox.org>
2024-07-05 09:30:57 +0800

    working 3.1.1

--- tests/chap3/chap3_1_1.sh
+++ tests/chap3/chap3_1_1.sh
@@ -5,9 +5,7 @@
 {
 grep_output=$(grep -Pqs '^\h*0\b' /sys/module/ipv6/parameters/disable && echo -e "\n - IPv6 is enabled\n" || echo -e "\n - IPv6 is not enabled\n")
 
-echo "grep output: [$grep_output]"
-
-if [[ "$grep_output" == $'\n - IPv6 is enabled\n' ]]; then
+if [[ "$grep_output" == $'\n - IPv6 is enabled' ]]; then
     echo "Audit Result: Pass"
 else
     echo "Audit Result: Fail"
