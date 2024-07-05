f730c43886d5bcb2dc6fab80c1d10225faca4d35
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-05 10:28:42 +0800

    fix: run 1.3 and 1.4 tests

--- runtests.sh
+++ runtests.sh
@@ -50,12 +50,14 @@ runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_6/*.sh
 echo "Testing config of /var/log/audit (1.1.2.7)..."
 runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_7/*.sh
 
+# 1.3 Mandatory Access Control
 echo "Testing Mandatory Access Control (1.3)..."
 echo "Testing AppArmor configuration (1.3.1)..."
 runTests ./tests/chap1/chap1_3/chap1_3_1/*.sh
 
-# 1.3 Mandatory Access Control
-# echo "Running Mandatory Access Control tests (Chapter 1.3)..."
+# 1.4 Testing configuration of bootloader
+echo "Testing config of bootloader (1.4)..."
+runTests "tests\chap1\chap1_4"
 
 # echo "Testing AppArmor configuration (1.3.1)..."
 # runTests ./tests/chap1/chap1_3/chap1_3_1/*.sh
