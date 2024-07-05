34a3e492e11d52b111c30b0a9716e030b9bec97e
Gabriel Limbaga <gabriel.limbaga00@gmail.com>
2024-07-05 10:13:02 +0800

    fixed 3.1.3

--- tests/chap3/chap3_1/chap3_1_3.sh
+++ tests/chap3/chap3_1/chap3_1_3.sh
@@ -6,5 +6,11 @@
     eservice=$(systemctl is-enabled bluetooth.service 2>/dev/null | grep 'enabled')
     aservice=$(systemctl is-active bluetooth.service 2>/dev/null | grep '^active')
     echo $eservice
-    echo $aservice
+    if [[ "$eservice" == "enabled"]]; then
+        systemctl stop bluetooth.service
+        systemctl mask bluetooth.service
+        echo "bluetooth.service is disabled"
+    else
+        echo "bluetooth.service is already disabled"
+    fi
 }
