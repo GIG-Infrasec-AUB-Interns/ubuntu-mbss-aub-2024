c4b3e0c7cb898ebf9f8c85af5c68c708e97d924b
Gabriel Limbaga <gabriel.limbaga00@gmail.com>
2024-07-05 09:28:06 +0800

    fixed 3.1.1

--- tests/chap3/chap3_1_1.sh
+++ tests/chap3/chap3_1_1.sh
@@ -5,7 +5,9 @@
 {
 grep_output=$(grep -Pqs '^\h*0\b' /sys/module/ipv6/parameters/disable && echo -e "\n - IPv6 is enabled\n" || echo -e "\n - IPv6 is not enabled\n")
 
-if [[ "$grep_output" == *" IPv6 is enabled"* ]]; then
+echo "grep output: [$grep_output]"
+
+if [[ "$grep_output" == $'\n - IPv6 is enabled\n' ]]; then
     echo "Audit Result: Pass"
 else
     echo "Audit Result: Fail"
