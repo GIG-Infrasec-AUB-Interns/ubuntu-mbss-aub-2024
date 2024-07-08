df2770ed0a0ec1381c2f3040bc2007fe46473d83
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-08 17:02:30 +0800

    feat: 1.7.7 - 1.7.10 test and remediation

---
+++ fixes/chap1/chap1_7/1_7_10.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+{
+    echo "[REMEDIATION] Ensuring XDMCP is not enabled..."
+
+    while IFS= read -r l_file; do
+        if grep -Pq '^\h*\[xdmcp\]' "$l_file"; then
+            echo "Processing file: $l_file"
+            # Comment out or remove the Enable=true line in the [xdmcp] block
+            sed -i '/\[xdmcp\]/,/\[/{/Enable=true/s/^/#/}' "$l_file"
+        fi
+    done < <(grep -Psil -- '^\h*\[xdmcp\]' /etc/{gdm3,gdm}/{custom,daemon}.conf)
+
+    echo "Remediation completed. XDMCP is now disabled."
+}
--- tests/chap1/chap1_7/1_7_8.sh
+++ tests/chap1/chap1_7/1_7_8.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/env bash
 {
-    echo "Ensuring GDM automatic mounting of removable media is disabled (1.7.6)..."
+    echo "Ensuring GDM autorun-never is enabled (1.7.8)..."
 
     l_pkgoutput="" l_output="" l_output2=""
     # Check if GNOME Desktop Manager is installed. If package isn't installed, recommendation is Not Applicable\n
     # determine system's package manager
-    if command -v dpkg-query > /dev/null 2>&1; then
+    if command -v dpkg-query &> /dev/null; then
         l_pq="dpkg-query -s"
-    elif command -v rpm > /dev/null 2>&1; then
+    elif command -v rpm &> /dev/null; then
         l_pq="rpm -q"
     fi
 
     # Check if GDM is installed
-    l_pcl="gdm gdm3" # Space seporated list of packages to check
+    l_pcl="gdm gdm3" # Space separated list of packages to check
     for l_pn in $l_pcl; do
-        $l_pq "$l_pn" > /dev/null 2>&1 && l_pkgoutput="$l_pkgoutput\n - Package: \"$l_pn\" exists on the system\n - checking configuration"
+        $l_pq "$l_pn" &> /dev/null && l_pkgoutput="$l_pkgoutput\n - Package: \"$l_pn\" exists on the system\n - checking configuration"
+        echo -e "$l_pkgoutput"
     done
 
     # Check configuration (If applicable)
     if [ -n "$l_pkgoutput" ]; then
         echo -e "$l_pkgoutput"
         # Look for existing settings and set variables if they exist
-        l_kfile="$(grep -Prils -- '^\h*automount\b' /etc/dconf/db/*.d)"
-        l_kfile2="$(grep -Prils -- '^\h*automount-open\b' /etc/dconf/db/*.d)"
+        l_kfile="$(grep -Prils -- '^\h*autorun-never\b' /etc/dconf/db/*.d)"
+        
         # Set profile name based on dconf db directory ({PROFILE_NAME}.d)
         if [ -f "$l_kfile" ]; then
             l_gpname="$(awk -F\/ '{split($(NF-1),a,".");print a[1]}' <<< "$l_kfile")"
-        elif [ -f "$l_kfile2" ]; then
-            l_gpname="$(awk -F\/ '{split($(NF-1),a,".");print a[1]}' <<< "$l_kfile2")"
         fi
 
         # If the profile name exist, continue checks
@@ -39,7 +38,7 @@
             else
                 l_output2="$l_output2\n - dconf database profile isn't set"
             fi
-
+            
             # Check if the dconf database file exists
             if [ -f "/etc/dconf/db/$l_gpname" ]; then
                 l_output="$l_output\n - The dconf database \"$l_gpname\" exists"
@@ -54,23 +53,15 @@
                 l_output2="$l_output2\n - The dconf directory \"$l_gpdir\" doesn't exist"
             fi
 
-            # check automount setting
-            if grep -Pqrs -- '^\h*automount\h*=\h*false\b' "$l_kfile"; then
-                l_output="$l_output\n - \"automount\" is set to false in: \"$l_kfile\""
+            # check autorun-never setting
+            if grep -Pqrs -- '^\h*autorun-never\h*=\h*true\b' "$l_kfile"; then
+                l_output="$l_output\n - \"autorun-never\" is set to true in: \"$l_kfile\""
             else
-                l_output2="$l_output2\n - \"automount\" is not set correctly"
+                l_output2="$l_output2\n - \"autorun-never\" is not set correctly"
             fi
-
-            # check automount-open setting
-            if grep -Pqs -- '^\h*automount-open\h*=\h*false\b' "$l_kfile2"; then
-                l_output="$l_output\n - \"automount-open\" is set to false in: \"$l_kfile2\""
-            else
-                l_output2="$l_output2\n - \"automount-open\" is not set correctly"
-            fi
-
         else
-            # Setings don't exist. Nothing further to check
-            l_output2="$l_output2\n - neither \"automount\" or \"automount-open\" is set"
+            # Settings don't exist. Nothing further to check
+            l_output2="$l_output2\n - \"autorun-never\" is not set"
         fi
     else
         l_output="$l_output\n - GNOME Desktop Manager package is not installed on the system\n - Recommendation is not applicable"
@@ -83,12 +74,12 @@
         echo -e "\n- Audit Result:\n ** FAIL **\n - Reason(s) for audit failure:\n$l_output2\n"
         [ -n "$l_output" ] && echo -e "\n- Correctly set:\n$l_output\n"
 
-        read -p "Run remediation script for Test 1.7.6? (Y/N): " ANSWER
+        read -p "Run remediation script for Test 1.7.8? (Y/N): " ANSWER
         case $ANSWER in
             [Yy])
-                echo "Commencing remediation for Test 1.7.6..."
+                echo "Commencing remediation for Test 1.7.8..."
 
-                FIXES_SCRIPT="$(realpath fixes/chap1/chap1_7/1_7_6.sh)"
+                FIXES_SCRIPT="$(realpath fixes/chap1/chap1_7/1_7_8.sh)"
                 if [ -f "$FIXES_SCRIPT" ]; then
                     chmod +x "$FIXES_SCRIPT"
                     "$FIXES_SCRIPT"
