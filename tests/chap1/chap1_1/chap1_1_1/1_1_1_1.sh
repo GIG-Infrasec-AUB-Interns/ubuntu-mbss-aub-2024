3a9554088be2a075bbec73f9e32d5b1f851c67e4
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-08 17:22:46 +0800

    test import functions

--- tests/chap1/chap1_1/chap1_1_1/1_1_1_1.sh
+++ tests/chap1/chap1_1/chap1_1_1/1_1_1_1.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-
+source utils.sh
 # 1.1.1.1 Ensure cramfs kernel module is not available
 
 {
@@ -67,25 +67,6 @@
         echo -e "\n- Audit Result:\n ** FAIL **\n - Reason(s) for audit failure:\n$l_output2\n"
         [ -n "$l_output" ] && echo -e "\n- Correctly set:\n$l_output\n"
 
-        # Remediation
-        read -p "Run remediation script for Test 1.1.1.1? (Y/N): " ANSWER
-        case "$ANSWER" in
-            [Yy]*)
-                echo "Commencing remediation for Test 1.1.1.1..."
-
-                FIXES_SCRIPT="$(realpath fixes/chap1/chap1_1/chap1_1_1/1_1_1_1.sh)"
-                if [ -f "$FIXES_SCRIPT" ]; then
-                    chmod +x "$FIXES_SCRIPT"
-                    "$FIXES_SCRIPT"
-                else
-                    echo "Error: $FIXES_SCRIPT is not found."
-                fi
-                echo "For more information, please visit https://downloads.cisecurity.org/#/"
-                ;;
-            *)
-                echo "Remediation not commenced"
-                echo "For more information, please visit https://downloads.cisecurity.org/#/"
-                ;;
-        esac
+        runFix "1.1.1.1" fixes/chap1/chap1_1/chap1_1_1/1_1_1_1.sh
     fi
 }
