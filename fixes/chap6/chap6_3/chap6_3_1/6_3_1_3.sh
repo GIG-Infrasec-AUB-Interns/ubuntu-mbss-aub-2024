a2601a3c70805d8e283ba36bbbe6858aa23c0084
igsanceda <igsanceda@up.edu.ph>
2024-07-09 15:11:03 +0800

    refactor: 6.3.1.3 remediation

--- fixes/chap6/chap6_3/chap6_3_1/6_3_1_3.sh
+++ fixes/chap6/chap6_3/chap6_3_1/6_3_1_3.sh
@@ -4,11 +4,13 @@
 
 { 
   echo "[REMEDIATION] Ensuring auditing for processes that start prior to auditd is enabled (6.3.1.3)..."
+
   GRUB_CONFIG_FILE="/etc/default/grub"
+  
   if grep -q '^GRUB_CMDLINE_LINUX=' "$GRUB_CONFIG_FILE"; then
-    sed -i 's/\(^GRUB_CMDLINE_LINUX=".*\)"/\1 audit=1"/' "$GRUB_CONFIG_FILE"
+    sed -i "s/\(^GRUB_CMDLINE_LINUX=\".*\)\"/\1 audit=1\"/" "$GRUB_CONFIG_FILE"
   else
-    echo 'GRUB_CMDLINE_LINUX="audit=1"' >> "$GRUB_CONFIG_FILE"
+    echo "GRUB_CMDLINE_LINUX=\"audit=1\"" >> "$GRUB_CONFIG_FILE"
   fi
 
   update-grub
