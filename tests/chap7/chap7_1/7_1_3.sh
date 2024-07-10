409420cedf52767ddcbf7d1404d0aab3738c6a4b
igsanceda <igsanceda@up.edu.ph>
2024-07-10 10:56:21 +0800

    feat: 7.1.3 test and remediation

---
+++ tests/chap7/chap7_1/7_1_3.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+source utils.sh
+# 7.1.3 Ensure permissions on /etc/group are configured
+
+{
+  echo "Ensuring permissions on /etc/group are configured (7.1.3)..."
+  audit_output=$(stat -Lc 'Access: (%#a) Uid: (%u/%U) Gid: (%g/%G)' /etc/group)
+
+  permissions=$(echo $audit_output | awk '{print $2}' | tr -d '()%#')
+  uid=$(echo $audit_output | awk '{print $4}' | tr -d '()%#')
+  gid=$(echo $audit_output | awk '{print $6}' | tr -d '()%#')
+
+  if ([ $permissions -le 0644 ] && [ "$uid" == "0/root" ] && [ "$gid" == "0/root" ]); then
+    echo "Audit output:"
+    echo "$audit_output"
+    echo "Audit Result: PASS"
+  else
+    echo "Audit output:"
+    echo "$audit_output"
+    echo "Audit Result: FAIL"
+  
+    runFix "7.1.1" fixes/chap7/chap7_1/7_1_1.sh # Remediation
+  fi
+}
