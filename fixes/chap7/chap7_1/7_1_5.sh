5ff1633e3f36c60ac606d84e7109004012e94237
igsanceda <igsanceda@up.edu.ph>
2024-07-10 11:17:56 +0800

    fix: set correct file in 7.1.5 and 7.1.6 remediation

--- fixes/chap7/chap7_1/7_1_5.sh
+++ fixes/chap7/chap7_1/7_1_5.sh
@@ -7,9 +7,9 @@
   
   chmod u-x,g-wx,o-rwx /etc/shadow
 
-  # Set ownership of etc/shadow to root OR shadow
+  # Set ownership of /etc/shadow to root OR shadow
   chown root:shadow /etc/shadow
-  # chown root:root /etc/root
+  # chown root:root /etc/shadow
 
   echo "Permissions on /etc/shadow are now configured."
 }
