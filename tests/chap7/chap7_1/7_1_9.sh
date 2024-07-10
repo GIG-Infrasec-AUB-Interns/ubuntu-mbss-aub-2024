e5e4b7778cf30540dd9b7715b3991f6da9e92f17
igsanceda <igsanceda@up.edu.ph>
2024-07-10 11:27:49 +0800

    feat: 7.1.9 test and remediation

---
+++ tests/chap7/chap7_1/7_1_9.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+source utils.sh
+# 7.1.9 Ensure permissions on /etc/shells are configured
+
+{
+  echo "Ensuring permissions on /etc/shells are configured (7.1.9)..."
+  audit_output=$(stat -Lc 'Access: (%#a) Uid: (%u/%U) Gid: (%g/%G)' /etc/shells)
+
+  permissions=$(echo $audit_output | awk '{print $2}' | tr -d '()%#')
+  uid=$(echo $audit_output | awk '{print $4}' | tr -d '()%#')
+  gid=$(echo $audit_output | awk '{print $6}' | tr -d '()%#')
+
+  if ([ $permissions -le 0644 ] && [ "$uid" == "0/root" ] && [ "$gid" == "0/root" ]); then
+    echo "Audit output:"
+    echo "$audit_output"
+    echo "Audit Result: PASS"
+  else
+    echo "Audit output:"
+    echo "$audit_output"
+    echo "Audit Result: FAIL"
+  
+    runFix "7.1.9" fixes/chap7/chap7_1/7_1_9.sh # Remediation
+  fi
+}
