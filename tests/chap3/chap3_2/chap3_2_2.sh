ff486b84926c1413ec3385b1d61bf24461dc4af6
Gabriel Limbaga <gabriel.limbaga00@gmail.com>
2024-07-10 10:32:07 +0800

    added remediation for 3.2.2, 3.2.3, 3.2.4

--- tests/chap3/chap3_2/chap3_2_2.sh
+++ tests/chap3/chap3_2/chap3_2_2.sh
@@ -58,5 +58,26 @@
     else
         echo -e "\n- Audit Result:\n ** FAIL **\n - Reason(s) for audit failure:\n$l_output2\n"
         [ -n "$l_output" ] && echo -e "\n- Correctly set:\n$l_output\n"
+
+        #Remediation 
+        read -p "Run remediation script for Test 3.2.2? (Y/N): " ANSWER
+        case "$ANSWER" in
+            [Yy]*)
+                echo "Commencing remediation for Test 3.2.2..."
+                # Resolve absolute path to fixes/chap3/chap3_2/chap3_2_2.sh
+                FIXES_SCRIPT="$(realpath fixes/chap3/chap3_2/chap3_2_2.sh)"
+                if [ -f "$FIXES_SCRIPT" ]; then
+                    chmod +x "$FIXES_SCRIPT"
+                    "$FIXES_SCRIPT"
+                else
+                    echo "Error: $FIXES_SCRIPT is not found."
+                fi
+                echo "For more information, please visit https://downloads.cisecurity.org/#/"
+                ;;
+            *)
+                echo "Remediation not commenced"
+                echo "For more information, please visit https://downloads.cisecurity.org/#/"
+                ;;
+        esac
     fi
 }
