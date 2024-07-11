55af269d9fdc685ee6fb0052fca5533c384156f6
igsanceda <igsanceda@up.edu.ph>
2024-07-11 15:21:36 +0800

    feat: 6.3.3.7 remediation

---
+++ fixes/chap6/chap6_3/chap6_3_3/6_3_3_7.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+# 6.3.3.7 [REMEDIATION] Ensure unsuccessful file access attempts are collected
+
+{
+    echo "[REMEDIATION] Ensuring unsuccessful file access attempts are collected (6.3.3.7)..."
+
+    UID_MIN=$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs) 
+    rules=(
+        "-a always,exit -F arch=b64 -S creat,open,openat,truncate,ftruncate -F exit=-EACCES -F auid>=${UID_MIN} -F auid!=unset -k access" 
+        "-a always,exit -F arch=b64 -S creat,open,openat,truncate,ftruncate -F exit=-EPERM -F auid>=${UID_MIN} -F auid!=unset -k access" 
+        "-a always,exit -F arch=b32 -S creat,open,openat,truncate,ftruncate -F exit=-EACCES -F auid>=${UID_MIN} -F auid!=unset -k access" 
+        "-a always,exit -F arch=b32 -S creat,open,openat,truncate,ftruncate -F exit=-EPERM -F auid>=${UID_MIN} -F auid!=unset -k access"
+    )
+    [ -n "${UID_MIN}" ] && printf "${rules[@]}" >> /etc/audit/rules.d/50-access.rules || printf "ERROR: Variable 'UID_MIN' is unset.\n"
+
+    echo "Unsuccessful file access attempts are now collected."
+}
