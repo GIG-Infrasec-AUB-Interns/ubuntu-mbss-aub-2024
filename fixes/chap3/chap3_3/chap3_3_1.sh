20895a2e4bedfdbc0db4c53f5650f5a5a4fd9a54
Gabriel Limbaga <gabriel.limbaga00@gmail.com>
2024-07-12 10:53:29 +0800

    added remediation for 3.3.1

--- fixes/chap3/chap3_3/chap3_3_1.sh
+++ fixes/chap3/chap3_3/chap3_3_1.sh
@@ -1,1 +1,15 @@
-#add
+
+{
+    #Set net.ipv4.ip_forward = 0 in /etc/sysctl.conf or a file in /etc/sysctl.d/ ending in .conf
+    printf '%s\n' "net.ipv4.ip_forward = 0" >> /etc/sysctl.d/60- netipv4_sysctl.conf
+
+    sysctl -w net.ipv4.ip_forward=0
+    sysctl -w net.ipv4.route.flush=1
+
+    #Set net.ipv6.conf.all.forwarding = 0 in /etc/sysctl.conf or a file in /etc/sysctl.d/ ending in .conf
+    printf '%s\n' "net.ipv6.conf.all.forwarding = 0" >> /etc/sysctl.d/60- netipv6_sysctl.conf
+
+    sysctl -w net.ipv6.conf.all.forwarding=0
+    sysctl -w net.ipv6.route.flush=1
+
+}
