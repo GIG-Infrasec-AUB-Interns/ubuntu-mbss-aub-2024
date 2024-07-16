2014afaed3e371d4b53d996512f7769f37d75083
Gabriel Aldrich Calubayan <gscalubayan@up.edu.ph>
2024-07-16 18:01:04 +0800

    feat: 5.4.2.2

---
+++ tests/chap5/chap5_4/chap5_4_2/5_4_2_2.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+source utils.sh
+source globals.sh
+
+{
+    echo "Ensure root is the only GID 0 account (5.4.2.2)..."
+
+    gid_0_users=$(awk -F: '($1 !~ /^(sync|shutdown|halt|operator)/ && $4=="0") {print $1":"$4}' /etc/passwd )
+
+    if [[ $gid_0_users == "root:0" ]]; then
+        echo "PASS"
+    else
+        echo "FAIL"
+        runFix "5.4.2.2" fixes/chap5/chap5_4/chap5_4_2/5_4_2_2.sh
+    fi
+}
