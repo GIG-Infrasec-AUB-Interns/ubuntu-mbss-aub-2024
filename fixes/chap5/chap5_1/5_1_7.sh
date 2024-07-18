1bc3f796b0b631c454680efd5da9874e00df26ba
Gabriel Limbaga <gabriel.limbaga00@gmail.com>
2024-07-18 11:38:07 +0800

    tried fixing 5.1.7

--- fixes/chap5/chap5_1/5_1_7.sh
+++ fixes/chap5/chap5_1/5_1_7.sh
@@ -1,8 +1,6 @@
 client_alive_interval="ClientAliveInterval 15"
 client_alive_count_max="ClientAliveCountMax 3"
 
-sudo sed -i '/^\s*ClientAliveInterval\b/d' /etc/ssh/sshd_config
-sudo sed -i '/^\s*ClientAliveCountMax\b/d' /etc/ssh/sshd_config
 
-sudo sed -i "/^\s*Include\b/i $client_alive_interval" /etc/ssh/sshd_config
-sudo sed -i "/^\s*Include\b/i $client_alive_count_max" /etc/ssh/sshd_config
+sudo sed -i "s/^\s*ClientAliveInterval\b.*/$client_alive_interval/" /etc/ssh/sshd_config
+sudo sed -i "s/^\s*ClientAliveCountMax\b.*/$client_alive_count_max/" /etc/ssh/sshd_config
