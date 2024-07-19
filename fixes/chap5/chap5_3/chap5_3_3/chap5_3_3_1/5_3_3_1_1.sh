a9cccd17f5a04c47f6dd1755e91233b8d0c92918
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-19 14:55:53 +0800

    fix: 5.3.3.1.1 attempt

--- fixes/chap5/chap5_3/chap5_3_3/chap5_3_3_1/5_3_3_1_1.sh
+++ fixes/chap5/chap5_3/chap5_3_3/chap5_3_3_1/5_3_3_1_1.sh
@@ -5,8 +5,8 @@ source utils.sh
     echo "[REMEDIATION] Ensure password failed attempts lockout is configured (5.3.3.1.1)..."
 
     # Set deny setting in /etc/security/faillock.conf to 5
-    if grep -q "deny" /etc/security/faillock.conf; then
-        sed -i 's/^\s*deny\s*=.*/deny = 5/' /etc/security/faillock.conf
+    if grep -q "^\s*#\?\s*deny\s*=" /etc/security/faillock.conf; then
+        sed -i 's/^\s*#\?\s*deny\s*=.*/deny = 5/' /etc/security/faillock.conf
     else
         echo "deny = 5" >> /etc/security/faillock.conf
     fi
