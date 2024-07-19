69f9e61df79ff8cdb8a4b14b07a216b88411a066
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-19 13:49:51 +0800

    feat: add tests in runDemo.sh

--- runDemo.sh
+++ runDemo.sh
@@ -14,12 +14,26 @@ echo "Ubuntu 22.04.4 Benchmark Tests DEMO"
 echo "Developed by Group GIG (InfraSec Team): Gabriel Calubayan, Ieiaiel Sanceda, Gabriel Limbaga"
 echo ""
 
+# test 1.7.3 (user list)
+echo "Partially auditing GNOME Display Manager configuration (1.7)..."
+# login banner
+echo "Auditing GDM login banner (1.7.2)..."
+./tests/chap1/chap1_7/1_7_2.sh
+# disable user list
+echo "Checking if GDM disable-user-list option is enabled (1.7.3)..."
+./tests/chap1/chap1_7/1_7_3.sh
+
 # Chapter 5.3
-echo "Running pam_faillock module configuration tests (Chapter 5.3.3.1)..."
+echo "Partially testing Pluggable Authentication Modules (Chapter 5.3)..."
+echo "Running PAM software package configuration tests (Chapter 5.3.1)..."
+runTests ./tests/chap5/chap5_3/chap5_3_1/*.sh
+
+echo "Testing pam-auth-update profiles (Chapter 5.3.2)..."
+runTests ./tests/chap5/chap5_3/chap5_3_2/*.sh
+
+echo "Partially testing PAM arguments (Chapter 5.3.3)..."
+echo "Running PAM software package configuration tests (Chapter 5.3.3.1)..."
 runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_1/*.sh
 
 echo "Running pam_pwquality module configuration tests (Chapter 5.3.3.2)..."
 runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_2/*.sh
-
-echo "Running pam_pwhistory module configuration tests (Chapter 5.3.3.3)..."
-runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_3/*.sh
