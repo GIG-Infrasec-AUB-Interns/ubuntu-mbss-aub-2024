82fb64b409d0e01feaae88657d146416526cf966
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-19 13:42:40 +0800

    chore: correct directories in runTests.sh

--- runtests.sh
+++ runtests.sh
@@ -50,11 +50,14 @@ runTests ./tests/chap1/chap1_1/chap1_1_2/chap1_1_2_7/*.sh
 # runTests ./tests/chap1/chap1_4/*.sh
 
 # 1.5 tests
+echo "Running Additional Process Hardening configuration tests (1.5)..."
 runTests ./tests/chap1/chap1_5/*.sh
 
 # 1.6 tests not done because it needs specific text from the organization approved by the legal department
 
 # 1.7 tests
+echo "Running GNOME Display Manager configuration tests (1.7)..."
+
 if check_gdm_installed; then
     echo "GDM is installed."
     echo "Running GDM tests..."
@@ -108,22 +111,10 @@ else
 fi
 
 # Chapter 3
-# 3.1 Network device configuration
-echo "Running Network device configuration (Chapter 3.1)..."
-runTests ./tests/chap3/chap3_1/*.sh
-echo "Running Network kernel module configuration(Chapter 3.2)..."
-runTests ./tests/chap3/chap3_2/*.sh
-
-#runTests for 3.3 is currently not working (results in nonterminating condition and skipping to the output for 3.3.10)
-#runTests ./tests/chap3/chap3_3/*.sh
-
-echo "Running UncomplicatedFirewall configuration (Chapter 4.1)..."
-runTests ./tests/chap4/chap4_1/*.sh
 
 # 3.1 Network device configuration
 echo "Running Network device configuration (Chapter 3.1)..."
 runTests ./tests/chap3/chap3_1/*.sh
-
 
 echo "Running Network kernel module configuration(Chapter 3.2)..."
 runTests ./tests/chap3/chap3_2/*.sh
@@ -133,20 +124,41 @@ runTests ./tests/chap3/chap3_2/*.sh
 
 # Chapter 4
 # 4.1 UncomplicatedFirewall configuration
-echo "Running UncomplicatedFirewall configuration (Chapter 4.1)..."
+echo "Running UncomplicatedFirewall configuration tests (Chapter 4.1)..."
 runTests ./tests/chap4/chap4_1/*.sh
 # for 4.1, 4.1.4 - 4.1.6 were not tested since they needed input for site policies which is unavailable to the team at the moment.
 # runTests ./tests/chap4/chap4_2/*.sh
 
 # for 4.2 there is an icmpv6 parsing error in the setup file so we may also not test it.
 
+echo "Testing iptable configuration (Chapter 4.3)..."
+
+echo "Running iptable software configuration tests (Chapter 4.3.1)..."
 runTests ./tests/chap4/chap4_3/chap4_3_1/*.sh
 
+echo "Running IPv4 iptable configuration tests (Chapter 4.3.2)..."
 runTests ./tests/chap4/chap4_3/chap4_3_2/*.sh
+
+echo "Running IPv6 iptable configuration tests (Chapter 4.3.3)..."
 runTests ./tests/chap4/chap4_3/chap4_3_3/*.sh
+
 # Chapter 5
 
-echo "Running pam_faillock module configuration tests (Chapter 5.3.3.1)..."
+# echo "Running SSH Server configuration tests (Chapter 5.1)..."
+# runTests ./tests/chap5/chap5_1/*.sh
+
+# echo "Running privilege escalation configuration tests (Chapter 5.2)..."
+# runTests ./tests/chap5/chap5_2/*.sh
+
+echo "Testing Pluggable Authentication Modules (Chapter 5.3)..."
+echo "Running PAM software package configuration tests (Chapter 5.3.1)..."
+runTests ./tests/chap5/chap5_3/chap5_3_1/*.sh
+
+echo "Testing pam-auth-update profiles (Chapter 5.3.2)..."
+runTests ./tests/chap5/chap5_3/chap5_3_2/*.sh
+
+echo "Testing PAM arguments (Chapter 5.3.3)..."
+echo "Running PAM software package configuration tests (Chapter 5.3.3.1)..."
 runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_1/*.sh
 
 echo "Running pam_pwquality module configuration tests (Chapter 5.3.3.2)..."
@@ -154,6 +166,9 @@ runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_2/*.sh
 
 echo "Running pam_pwhistory module configuration tests (Chapter 5.3.3.3)..."
 runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_3/*.sh
+
+echo "Running pam_unix module module configuration tests (Chapter 5.3.3.4)..."
+runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_4/*.sh
 
 echo "Running shadow password suite parameter configuration tests (Chapter 5.4.1)..."
 runTests ./tests/chap5/chap5_4/chap5_4_1/*.sh
@@ -164,3 +179,35 @@ runTests ./tests/chap5/chap5_4/chap5_4_2/*.sh
 echo "Running user default environment configuration tests (Chapter 5.4.3)..."
 runTests ./tests/chap5/chap5_4/chap5_4_3/*.sh
 
+# Chapter 6
+
+echo "Running Filesystem Integrity Checking configuration tests (Chapter 6.1)..."
+runTests ./tests/chap6/chap6_1/*.sh
+
+echo "Running systemd-journald service configuration tests (Chapter 6.2.1.1)..."
+runTests ./tests/chap6/chap6_2/chap6_2_1/chap6_2_1_1/*.sh
+
+echo "Running systemd-journal-remote configuration tests (Chapter 6.2.1.2)..."
+runTests ./tests/chap6/chap6_2/chap6_2_1/chap6_2_1_2/*.sh
+
+echo "Running Logfiles configuration tests (Chapter 6.2.2)..."
+runTests ./tests/chap6/chap6_2/chap6_2_2/*.sh
+
+echo "Running auditd Service configuration tests (Chapter 6.3.1)..."
+runTests ./tests/chap6/chap6_3/chap6_3_1/*.sh
+
+echo "Running Data Retention configuration tests (Chapter 6.3.2)..."
+runTests ./tests/chap6/chap6_3/chap6_3_2/*.sh
+
+echo "Running auditd Rule configuration tests (Chapter 6.3.3)..."
+runTests ./tests/chap6/chap6_3/chap6_3_3/*.sh
+
+echo "Running auditd File Access configuration tests (Chapter 6.3.4)..."
+runTests ./tests/chap6/chap6_3/chap6_3_4/*.sh
+
+# Chapter 7
+echo "Running System File Permission configuration tests (Chapter 7.1)..."
+runTests ./tests/chap7/chap7_1/*.sh
+
+echo "Running Local User and Group Setting configuration tests (Chapter 7.1)..."
+runTests ./tests/chap7/chap7_2/*.sh
