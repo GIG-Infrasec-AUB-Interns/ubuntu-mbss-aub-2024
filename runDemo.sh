3a6c3dad7522f9b9f8b89f43a230dec4965466ce
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-07-19 11:16:01 +0800

    docs: uncomment runTests calls in runTests.sh, change dir of runDemo

--- runDemo.sh
+++ runDemo.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
-source ./utils.sh
-source ./globals.sh
+source ./demo/utils.sh
+source ./demo/globals.sh
 # Benchmark tests on Ubuntu 22.04.4 based on
 # CIS Benchmarks https://downloads.cisecurity.org/#/
 
@@ -8,19 +8,20 @@ source ./globals.sh
 
 # su root
 # chmod +x runtests.sh 
-# ./runtests.sh
+# ./demo/runDemo.sh
 
-echo "Ubuntu 22.04.4 Benchmark Tests based on CIS Benchmarks"
-echo "Developed by AUB Interns: Gabriel Calubayan, Ieiaiel Sanceda, Gabriel Limbaga"
+echo "Ubuntu 22.04.4 Benchmark Tests DEMO"
+echo "Developed by Group GIG (InfraSec Team): Gabriel Calubayan, Ieiaiel Sanceda, Gabriel Limbaga"
 echo ""
 
 # Chapter 5
 
 echo "Running pam_faillock module configuration tests (Chapter 5.3.3.1)..."
-runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_1/*.sh
+
+runTests ./demo/tests/chap5/chap5_3/chap5_3_3/chap5_3_3_1/*.sh
 
 echo "Running pam_pwquality module configuration tests (Chapter 5.3.3.2)..."
-runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_2/*.sh
+runTests ./demo/tests/chap5/chap5_3/chap5_3_3/chap5_3_3_2/*.sh
 
 echo "Running pam_pwhistory module configuration tests (Chapter 5.3.3.3)..."
-runTests ./tests/chap5/chap5_3/chap5_3_3/chap5_3_3_3/*.sh
+runTests ./demo/tests/chap5/chap5_3/chap5_3_3/chap5_3_3_3/*.sh
